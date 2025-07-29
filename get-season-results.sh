e55b703b560c15874a77118953674d07369e7d63
John Konecny <24961694+jfkonecn@users.noreply.github.com>
2025-07-29 07:47:29 -0400

    Initial Commit

--- get-season-results.sh
+++ get-season-results.sh
@@ -48,3 +48,17 @@ URL_PRO_TEAMS="https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/$
 # Download the data into the data directory
 curl -sSL \
   "$URL_PRO_TEAMS" -o "data/espn_pro_teams_${YEAR}.json"
+
+
+URL_PLAYER_INFO="https://lm-api-reads.fantasy.espn.com/apis/v3/games/ffl/seasons/$YEAR/segments/0/leagues/$ESPN_LEAGUE_ID?view=kona_player_info"
+
+# Download the data into the data directory
+curl -sSL \
+    -H "Cookie: $COOKIE" \
+    -H 'x-fantasy-filter: {"players":{"filterSlotIds":null,"sortPercChanged":{"sortPriority":1,"sortAsc":true},"limit":25,"filterRanksForSlotIds":{"value":[0,2,4,6,17,16,8,9,10,12,13,24,11,14,15]},"filterStatsForTopScoringPeriodIds":{"value":2,"additionalValue":["002022","102022","002021","022022"]}}}' \
+  "$URL_PLAYER_INFO" -o "data/espn_most_dropped_${YEAR}.json"
+
+curl -sSL \
+    -H "Cookie: $COOKIE" \
+    -H 'x-fantasy-filter: {"players":{"filterSlotIds":null,"sortPercChanged":{"sortPriority":1,"sortAsc":false},"limit":25,"filterRanksForSlotIds":{"value":[0,2,4,6,17,16,8,9,10,12,13,24,11,14,15]},"filterStatsForTopScoringPeriodIds":{"value":2,"additionalValue":["002022","102022","002021","022022"]}}}' \
+  "$URL_PLAYER_INFO" -o "data/espn_most_added_${YEAR}.json"
